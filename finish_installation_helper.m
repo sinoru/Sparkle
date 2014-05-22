ff56d44287170783053ed4e135b1db7caa45bba5
Sinoru Dev <sinoru.dev@me.com>
2014-05-22 22:26:02 +0900

    Fix some attribute issue

--- finish_installation_helper.m
+++ finish_installation_helper.m
@@ -1,11 +1,6 @@
 
 #import <AppKit/AppKit.h>
-#import "SUInstaller.h"
-#import "SUHost.h"
-#import "SUStandardVersionComparator.h"
-#import "SUStatusController.h"
-#import "SUPlainInstallerInternals.h"
-#import "SULog.h"
+#import <sys/xattr.h>
 
 static void new_connection_handler(xpc_connection_t peer)
 {
@@ -23,6 +18,13 @@ static void new_connection_handler(xpc_connection_t peer)
             
             if( xpc_array_get_count(array) < 5 || xpc_array_get_count(array) > 6 )
                 return;
+            
+            NSString *relaunchToolPath = [NSString stringWithUTF8String:xpc_array_get_string(array, 0)];
+            
+            int err = removexattr(relaunchToolPath.UTF8String, "com.apple.quarantine", 0);
+            if ( err == -1 ) {
+                printf( "removexattr failed with error %d - \"%s\" \n", errno, strerror( errno) );
+            }
             
             [NSTask launchedTaskWithLaunchPath: [NSString stringWithUTF8String:xpc_array_get_string(array, 0)] arguments:[NSArray arrayWithObjects:[NSString stringWithUTF8String:xpc_array_get_string(array, 1)], [NSString stringWithUTF8String:xpc_array_get_string(array, 2)], [NSString stringWithUTF8String:xpc_array_get_string(array, 3)], [NSString stringWithUTF8String:xpc_array_get_string(array, 4)], xpc_array_get_bool(array, 5) ? @"1" : @"0", nil]];
             
